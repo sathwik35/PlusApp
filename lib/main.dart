7d956676909cffab082700b1613fb12b977fa7e8
Timmeh <timoldenburger@gmail.com>
2018-06-05 16:43:00 +0200

    Removed debuglabel topcorner

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: new ThemeData(primaryColor: new Color(0xFF81BD1D)),
       home: new LoginPage(),
     );
